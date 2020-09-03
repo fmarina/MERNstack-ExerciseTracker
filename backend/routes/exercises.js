757787e6e98bd0d4225b18456e7f4e6b1e2f7681
Marina <marinafaenze@hotmail.com>
2020-09-02 21:48:50 -0300

    Delete, Post and Get by id methods added

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -25,4 +25,34 @@ router.route('/add').post((req, res) => {
       .catch(err => res.status(400).json('Error ' + err));
 });
 
+router.route('/:id').get((req, res) => {
+  const id = req.params.id;
+  Exercise.findById(id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json('Error ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+  const id = req.params.id
+  Exercise.findByIdAndDelete(id)
+    .then(() => res.json('Exercise deleted!'))
+    .catch(err => res.status(400).json('Error ' + err));
+})
+
+router.route('/update/:id').post((req, res) => {
+  const id = req.params.id;
+  Exercise.findById(id)
+    .then(exercise => {
+      exercise.username = req.body.username
+      exercise.description = req.body.description
+      exercise.duration = Number(req.body.duration);
+      exercise.date = Data.parse(req.body.date);
+
+      exercise.save()
+        .then(() => res.json('Exercise updated!'))
+        .catch(err => res.status(400).json('Error ' + err));
+    })
+    .catch(err => res.status(400).json('Error ' + err));
+});
+
 module.exports = router;
