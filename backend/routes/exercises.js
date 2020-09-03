141d44fbdfb9953619a377b9cc16246e2c46f7bc
Marina <marinafaenze@hotmail.com>
2020-09-03 11:48:29 -0300

    fix Date.parse

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -46,7 +46,7 @@ router.route('/update/:id').post((req, res) => {
       exercise.username = req.body.username
       exercise.description = req.body.description
       exercise.duration = Number(req.body.duration);
-      exercise.date = Data.parse(req.body.date);
+      exercise.date = Date.parse(req.body.date);
 
       exercise.save()
         .then(() => res.json('Exercise updated!'))
