f79636b0c785977f0abc8954c86025d1277b15c0
Marina <marinafaenze@hotmail.com>
2020-09-03 17:29:36 -0300

    Navbar Component created

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return(
+    <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+      <Link to="/" className="navbar-brand">Excercise Tracker</Link>
+      <div className="collpase navbar-collapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
+            <Link to="/" className="nav-link">Excercises</Link>
+          </li>
+          <li className="navbar-item" className="nav-link">
+            <Link to="/create">Create Excercise Log</Link>
+          </li>
+          <li className="navbar-item" className="nav-link">
+            <Link to="/user">Create User</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
