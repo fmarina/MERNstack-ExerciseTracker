02f833b784e0dbf15530119a20b847abfa9601bd
Marina <marinafaenze@hotmail.com>
2020-09-14 18:20:11 -0300

    render all exercises list

---
+++ src/components/Exercises/ExercisesList.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+
+const Exercise = ({exercise, deleteExercise}) => (
+    <tr>
+        <td>{exercise.username}</td>
+        <td>{exercise.description}</td>
+        <td>{exercise.duration}</td>
+        <td>{exercise.date.substring(0, 10)}</td>
+        <td>
+            <Link to={`/edit/${exercise.id}`}>Edit </Link> 
+            | 
+            <a href="#" onClick={() => deleteExercise(exercise.id)}> Delete</a>
+        </td>
+    </tr>
+)
+
+const ExercisesList = () => {
+    const [exercises, setExercises] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/exercises/')
+          .then(response => setExercises(response.data))
+          .catch(err => console.log("Error ", err));
+    }, []);
+
+    const deleteExercise = (id) => {
+        axios.delete('http://localhost:5000/exercises/' + id)
+          .then(res => console.log(res.data));
+        
+        setExercises((prevExerc) => prevExerc.filter(exerc => exerc.id !== id));
+    }
+
+    const showExercises = () => (
+        exercises.map(currentExerc => (
+            <Exercise
+                key={currentExerc.id}
+                exercise={currentExerc} 
+                deleteExercise={deleteExercise}
+            />
+        ))
+    );
+
+    return (
+        <div>
+            <h2>Exercises List</h2>
+            <table className="table">
+                <thead className="thead-light">
+                    <tr>
+                        <th>Username</th>
+                        <th>Description</th>
+                        <th>Duration</th>
+                        <th>Date</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody> {showExercises()} </tbody>       
+            </table>
+        </div>
+    );
+}
+
+export default ExercisesList;
